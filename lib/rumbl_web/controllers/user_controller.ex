8e7e6a59980f21a8de36d764616c6fca0589e5c6
Benjamín Silva <silva96@gmail.com>
2018-09-14 12:02:32 -0300

    auth actions

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -5,8 +5,14 @@ defmodule RumblWeb.UserController do
   alias Rumbl.Accounts.User
 
   def index(conn, _params) do
-    users = Accounts.list_users()
-    render(conn, "index.html", users: users)
+    case authenticate(conn) do
+      %Plug.Conn{halted: true} = conn ->
+        conn
+
+      conn ->
+        users = Accounts.list_users()
+        render(conn, "index.html", users: users)
+    end
   end
 
   def show(conn, %{"id" => id}) do
@@ -23,6 +29,7 @@ defmodule RumblWeb.UserController do
     case Accounts.register_user(user_params) do
       {:ok, user} ->
         conn
+        |> RumblWeb.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: Routes.user_path(conn, :index))
 
@@ -31,4 +38,14 @@ defmodule RumblWeb.UserController do
     end
   end
 
+  defp authenticate(conn) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
+    end
+  end
 end
